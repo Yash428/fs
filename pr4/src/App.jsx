a14e551c2775ce3b93af9c1c6c86e928c88e9d00
Yash428 <yash42879@gmail.com>
2024-08-22 12:27:09 +0530

    pr5 done

--- pr4/src/App.jsx
+++ pr4/src/App.jsx
@@ -5,15 +5,26 @@ function App() {
   const [count, setCount] = useState(0)
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
+  const [fontFamily, setFontFamily] = useState('font-sans')
   return (
-    <div className='flex flex-col justify-center items-center h-screen bg-pink-500 w-full font-semibold'>
+    <div className={`flex flex-col justify-center items-center h-screen bg-pink-500 w-full font-semibold ${fontFamily}`}>
     <div className='flex flex-col justify-center w-full items-center'>
-      <div className='text-center text-3xl text-white font-bold p-3 m-2'>Count: {count} </div>
+      <div className='text-center flex flex-row items-center justify-center text-3xl text-white font-bold w-1/3 p-3 m-2'>
+        <div className='m-2 w-2/3'>Count: {count}</div>
+        <div className=' text-lg w-1/3 text-black '>
+          <select className=' bg-neutral-300 py-2 px-1 rounded-lg border-2 border-black' onChange={(e)=>setFontFamily(e.target.value)}>
+            <option value="">Select Font</option>
+            <option value="font-sans">Sans</option>
+            <option value="font-serif">Serif</option>
+            <option value="font-mono">mono</option>
+          </select>
+        </div>
+      </div>
       <div className=' flex flex-row w-1/2 items-center justify-center'>
-        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 border-black rounded-xl w-1/4' onClick={() => setCount(0)}>Reset</button>
-        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 border-black rounded-xl w-1/4' onClick={() => setCount(count + 1)}>Increment</button>
-        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 border-black rounded-xl w-1/4' onClick={() => setCount(count - 1)}>Decrement</button>
-        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 border-black rounded-xl w-1/4' onClick={() => setCount(count + 5)}>Increment 5</button>
+        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 text-black border-black text-lg rounded-xl w-1/4' onClick={() => setCount(0)}>Reset</button>
+        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 text-black text-lg border-black rounded-xl w-1/4' onClick={() => setCount(count + 1)}>Increment</button>
+        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 border-black text-lg text-black rounded-xl w-1/4' onClick={() => count>0?setCount(count - 1):null}>Decrement</button>
+        <button className='p-2 mx-1 my-2 border-2 bg-neutral-200 text-xl border-black rounded-xl w-1/4' onClick={() => setCount(count + 5)}>Increment 5</button>
       </div>
     </div>
     <div className='flex flex-col p-2 m-1 items-center justify-center'>
--- pr5/src/main.jsx
+++ pr5/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="flex flex-col items-center justify-center">
+      <div className="mt-6 font-bold p-2 text-3xl flex items-center justify-center text-white">Calculator using React App [22DCS085]</div>
+      <App />
+    </div>
+  </React.StrictMode>
+);
